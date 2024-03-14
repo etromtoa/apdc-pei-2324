2df40ede49ff9b66402ad82d3232aa012229fd3d
m. pauleta 41999 <m.pauleta@campus.fct.unl.pt>
2024-03-14 15:32:10 +0000

    restister v1 and stuff from the f4.2

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
@@ -0,0 +1,67 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreException;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.gson.Gson;
+
+import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+
+import java.util.logging.Logger;
+
+@Path("/register")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class RegisterResource {
+
+	private static final Logger LOG = Logger.getLogger(RegisterResource.class.getName());
+
+	private final Gson g = new Gson();
+
+	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+	public RegisterResource() {
+
+	}
+
+	@POST
+	@Path("/v1")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doRegister(LoginData data) {
+		LOG.fine("Registier attempt by user: " + data.username);
+		Key userKey = datastore.newKeyFactory().setKind("username").newKey("Carlos");
+		Entity person = Entity.newBuilder(userKey).set("password", "cd@fct.unl.pt").build();
+		try {
+			datastore.add(person);
+		} catch (DatastoreException ex) {
+
+			return Response.status(Status.FORBIDDEN).entity("Username already in use.").build();
+		}
+
+		AuthToken at = new AuthToken(data.username);
+		return Response.ok(g.toJson(at)).build();
+	}
+
+	@GET
+	@Path("/{username}")
+	public Response checkUsernameAvailable(@PathParam("username") String username) {
+		if (username.equals("jleitao")) {
+			return Response.ok().entity(g.toJson(false)).build();
+		} else {
+			return Response.ok().entity(g.toJson(true)).build();
+		}
+	}
+
+}
