08ed42fd3b3447b795a9d7e5d2b0689d6e24a048
m. pauleta 41999 <m.pauleta@campus.fct.unl.pt>
2024-03-15 03:01:47 +0000

    login funciona. god bless our souls

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -10,6 +10,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
 import com.google.gson.Gson;
 
 import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
@@ -25,6 +29,8 @@ public class LoginResource {
 
 	private final Gson g = new Gson();
 
+	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
 	public LoginResource() {
 
 	}
@@ -35,6 +41,29 @@ public class LoginResource {
 	public Response doLogin(LoginData data) {
 		LOG.fine("Login attempt by user: " + data.username);
 		if (data.username.equals("jleitao") && data.password.equals("password")) {
+			AuthToken at = new AuthToken(data.username);
+			return Response.ok(g.toJson(at)).build();
+		}
+		return Response.status(Status.FORBIDDEN).entity("Incorrect username or password.").build();
+	}
+
+	@POST
+	@Path("/v1")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doLoginV1(LoginData data) {
+		LOG.fine("Login attempt by user: " + data.username);
+		if (data.username == null || data.password == null) {
+			return Response.status(Status.BAD_REQUEST).entity("You forgot to fill at least a field.").build();
+		}
+		if (data.username.isEmpty() || data.password.isEmpty()) {
+			return Response.status(Status.BAD_REQUEST).entity("You must think you are really funny.").build();
+		}
+		Key userKey = datastore.newKeyFactory().setKind("username").newKey(data.username);
+
+		Entity temp = datastore.get(userKey);
+
+		if (temp.getString("password").equals(data.password)) {
+
 			AuthToken at = new AuthToken(data.username);
 			return Response.ok(g.toJson(at)).build();
 		}
--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
@@ -16,13 +16,11 @@ import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.gson.Gson;
-import com.google.protobuf.Timestamp;
 
 import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
 import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginDataV2;
 
-import java.time.Clock;
-import java.time.Instant;
 import java.util.logging.Logger;
 
 @Path("/register")
@@ -45,8 +43,43 @@ public class RegisterResource {
 	public Response doRegister(LoginData data) {
 		LOG.fine("Registier attempt by user: " + data.username);
 		Key userKey = datastore.newKeyFactory().setKind("username").newKey(data.username);
-		Entity person = Entity.newBuilder(userKey).set("password", data.password).build();
-		person.newBuilder(userKey).set("timeOfCreation", System.currentTimeMillis());
+		Entity person = Entity.newBuilder(userKey).set("password", data.password)
+				.set("timeOfCreation", System.currentTimeMillis()).build();
+
+		try {
+			datastore.add(person);
+		} catch (DatastoreException ex) {
+
+			return Response.status(Status.FORBIDDEN).entity("Username already in use.").build();
+		}
+
+		AuthToken at = new AuthToken(data.username);
+		return Response.ok(g.toJson(at)).build();
+	}
+
+	@POST
+	@Path("/v2")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doRegisterV2(LoginDataV2 data) {
+		LOG.fine("Registier attempt by user: " + data.username);
+		if (!data.password.equals(data.confirmation)) {
+
+			return Response.status(Status.FORBIDDEN).entity("Password mismatch.").build();
+		}
+
+		if (data.username == null || data.password == null || data.confirmation == null || data.email == null
+				|| data.name == null) {
+			return Response.status(Status.BAD_REQUEST).entity("You forgot to fill at least a field.").build();
+		}
+		if (data.username.isEmpty() || data.password.isEmpty() || data.confirmation.isEmpty() || data.email.isEmpty()
+				|| data.name.isEmpty()) {
+			return Response.status(Status.BAD_REQUEST).entity("You must think you are really funny.").build();
+		}
+		Key userKey = datastore.newKeyFactory().setKind("username").newKey(data.username);
+		Entity person = Entity.newBuilder(userKey).set("password", data.password)
+				.set("timeOfCreation", System.currentTimeMillis()).set("confirmation", data.confirmation)
+				.set("email", data.email).set("name", data.name).build();
+
 		try {
 			datastore.add(person);
 		} catch (DatastoreException ex) {
