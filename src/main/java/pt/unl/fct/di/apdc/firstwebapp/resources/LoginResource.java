547bbefd878a20cd8347855d159c77439016820e
m. pauleta 41999 <m.pauleta@campus.fct.unl.pt>
2024-03-17 03:16:39 +0000

    all tasks till f6 task 4 done

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -1,25 +1,38 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import com.google.cloud.Timestamp;
 import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreException;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.TimestampValue;
 import com.google.gson.Gson;
-
 import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
 import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+import pt.unl.fct.di.apdc.firstwebapp.util.Utils;
 
 import java.util.logging.Logger;
+import java.net.http.HttpHeaders;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -51,23 +64,115 @@ public class LoginResource {
 	@Path("/v1")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response doLoginV1(LoginData data) {
-		LOG.fine("Login attempt by user: " + data.username);
-		if (data.username == null || data.password == null) {
-			return Response.status(Status.BAD_REQUEST).entity("You forgot to fill at least a field.").build();
-		}
-		if (data.username.isEmpty() || data.password.isEmpty()) {
-			return Response.status(Status.BAD_REQUEST).entity("You must think you are really funny.").build();
-		}
-		Key userKey = datastore.newKeyFactory().setKind("username").newKey(data.username);
+		LOG.fine("Attemp to login user: " + data.username);
 
-		Entity temp = datastore.get(userKey);
-
-		if (temp.getString("password").equals(data.password)) {
-
-			AuthToken at = new AuthToken(data.username);
-			return Response.ok(g.toJson(at)).build();
+		String status = isDataValid(data);
+		if (!status.equals(Utils.SUCCESS)) {
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
 		}
-		return Response.status(Status.FORBIDDEN).entity("Incorrect username or password.").build();
+
+		Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
+		Entity entity = datastore.get(userKey);
+		if (entity != null) {
+			status = arePasswordsEqual(entity.getString("password"), DigestUtils.sha512Hex(data.password));
+			if (status.equals(Utils.SUCCESS)) {
+				TimestampValue now = TimestampValue.of(Timestamp.now());
+				List<TimestampValue> lastLogins = new ArrayList<TimestampValue>();
+				Date yesterday = Date.from(Instant.now().minusSeconds(24 * 3600));
+				try {
+					List<TimestampValue> tempList = entity.getList("lastlogins");
+					lastLogins.addAll(tempList);
+				} catch (DatastoreException e) {
+					LOG.fine("Welcome to your first log in");
+				}
+				lastLogins.add(now);
+				lastLogins.removeIf(t -> t.get().toDate().before(yesterday));
+				AuthToken at = new AuthToken(data.username);
+
+				Entity tempEntity = Entity.newBuilder(entity).set("lastlogins", lastLogins).set("lastLogin", now)
+						.build();
+				datastore.update(tempEntity);
+				return Response.ok(g.toJson(at)).build();
+			}
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
+		}
+		return Response.status(Status.NOT_FOUND).entity(Utils.USERNAME_NOT_EXISTS).build();
+	}
+
+	@POST
+	@Path("/v2")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doLoginV2(LoginData data, @Context HttpServletRequest request, @Context HttpHeaders headers) {
+		LOG.fine("Attemp to login user: " + data.username);
+
+		String status = isDataValid(data);
+		if (!status.equals(Utils.SUCCESS)) {
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
+		}
+
+		Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
+		Entity entity = datastore.get(userKey);
+		if (entity != null) {
+			status = arePasswordsEqual(entity.getString("password"), DigestUtils.sha512Hex(data.password));
+			if (status.equals(Utils.SUCCESS)) {
+				TimestampValue now = TimestampValue.of(Timestamp.now());
+				List<TimestampValue> lastLogins = new ArrayList<TimestampValue>();
+				Date yesterday = Date.from(Instant.now().minusSeconds(24 * 3600));
+				try {
+					List<TimestampValue> tempList = entity.getList("lastlogins");
+					lastLogins.addAll(tempList);
+				} catch (DatastoreException e) {
+					LOG.fine("Welcome to your first log in");
+				}
+				lastLogins.add(now);
+				lastLogins.removeIf(t -> t.get().toDate().before(yesterday));
+				AuthToken at = new AuthToken(data.username);
+
+				Entity tempEntity = Entity.newBuilder(entity).set("lastlogins", lastLogins).set("lastLogin", now)
+						.build();
+				datastore.update(tempEntity);
+				return Response.ok(g.toJson(at)).build();
+			}
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
+		}
+		return Response.status(Status.NOT_FOUND).entity(Utils.USERNAME_NOT_EXISTS).build();
+	}
+
+	@POST
+	@Path("/user")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response listLast24HourLogins(LoginData data) {
+		LOG.fine("Attemp to recall logins from user in the last 24 hours: " + data.username);
+
+		String status = isDataValid(data);
+		if (!status.equals(Utils.SUCCESS)) {
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
+		}
+
+		Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
+		Entity entity = datastore.get(userKey);
+		if (entity != null) {
+			status = arePasswordsEqual(entity.getString("password"), DigestUtils.sha512Hex(data.password));
+			if (status.equals(Utils.SUCCESS)) {
+				List<TimestampValue> lastLogins = new LinkedList<TimestampValue>();
+				Date yesterday = Date.from(Instant.now().minusSeconds(24 * 3600));
+				try {
+					List<TimestampValue> tempList = entity.getList("lastlogins");
+					lastLogins.addAll(tempList);
+				} catch (DatastoreException e) {
+					return Response.status(Status.EXPECTATION_FAILED).entity(Utils.USER_DIDNT_LOG_IN).build();
+				}
+				lastLogins.removeIf(t -> t.get().toDate().before(yesterday));
+
+				if (lastLogins.size() > 0) {
+					return Response.ok(g.toJson(lastLogins.toArray())).build();
+				} else {
+					return Response.status(Status.EXPECTATION_FAILED).entity(Utils.USER_DIDNT_LOG_IN).build();
+				}
+			}
+			return Response.status(Status.BAD_REQUEST).entity(status).build();
+		}
+		return Response.status(Status.NOT_FOUND).entity(Utils.USERNAME_NOT_EXISTS).build();
 	}
 
 	@GET
@@ -80,4 +185,43 @@ public class LoginResource {
 		}
 	}
 
+	private String areParamsNull(LoginData data) {
+		String status = Utils.SUCCESS;
+		if (Utils.isFieldNull(data.username) || Utils.isFieldNull(data.password))
+			status = Utils.FIELDS_NULL;
+		return status;
+	}
+
+	private String areParamsEmpty(LoginData data) {
+		String status = Utils.SUCCESS;
+		if (Utils.isFieldEmpty(data.username) || Utils.isFieldEmpty(data.password)) {
+			status = Utils.FIELDS_EMPTY;
+
+		}
+		return status;
+
+	}
+
+	private String arePasswordsEqual(String password1, String confirmation) {
+		String status = Utils.SUCCESS;
+		if (!Utils.areFieldsEqual(password1, confirmation)) {
+			status = Utils.PW_NO_MATCH;
+		}
+		return status;
+	}
+
+	private String isDataValid(LoginData data) {
+
+		String status = areParamsNull(data);
+		if (!status.equals(Utils.SUCCESS)) {
+			return status;
+		}
+
+		status = areParamsEmpty(data);
+		if (!status.equals(Utils.SUCCESS)) {
+			return status;
+		}
+
+		return Utils.SUCCESS;
+	}
 }
---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/Utils.java
@@ -0,0 +1,162 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utils {
+
+	// FIELD SIZES
+
+	public static final int USERNAME_MIN_LENGTH = 3;
+	public static final int USERNAME_MAX_LENGTH = 20;
+
+	public static final int PASSWORD_MIN_LENGTH = 5;
+	public static final int PASSWORD_MAX_LENGTH = 20;
+
+	public static final int EMAIL_MAX_LENGTH_LABELS = 63;
+	public static final int EMAIL_MAX_LENGTH_LOCAL = 64;
+	public static final int EMAIL_MAX_LENGTH_DOMAIN = 255;
+
+	public static final int NAME_MIN_LENGTH = 3;
+	public static final int NAME_MAX_LENGTH = 64;
+
+	public static final String SUCCESS = "SUCCESS";
+
+	// ERROR MESSAGES GENERAL
+
+	public static final String FIELDS_NULL = "At least one field is null.";
+	public static final String FIELDS_EMPTY = "At least one field is empty.";
+	public static final String PW_NO_MATCH = "Passwords don't match.";
+	public static final String USER_DIDNT_LOG_IN = "This user has not logged in in the last 24 hours.";
+
+	// ERROR MESSAGES USERNAME
+
+	public static final String WRONG_LENGTH_USERNAME = "Username must be between " + USERNAME_MIN_LENGTH + " and "
+			+ USERNAME_MAX_LENGTH + " characters long.";
+	public static final String USERNAME_IN_USE = "Username already in use.";
+	public static final String USERNAME_NOT_EXISTS = "Username does not exist, please register.";
+	public static final String BAD_USERNAME = "Please use only lower case letters, base ten Roman digits and the special characters (-_.) for the username";
+
+	// ERROR MESSAGES PASSWORD
+
+	public static final String WRONG_LENGTH_PASSWORD = "Password must be between " + PASSWORD_MIN_LENGTH + " and "
+			+ PASSWORD_MAX_LENGTH + " characters long.";
+	public static final String PASSWORD_NO_SPECIAL = "Password must contain at least one special character (!@#$%^&*(),.?\\\":{}|<>).";
+	public static final String PASSWORD_NO_CAPS = "Password must contain at least one Upper Case Letter.";
+	public static final String PASSWORD_NO_LOWKEY = "Password must contain at least one lower case Letter.";
+	public static final String PASSWORD_NO_NUMBER = "Password must contain at least one Base Ten Roman digit.";
+
+	// ERROR MESSAGES EMAIL
+
+	public static final String BAD_EMAIL = "Please provide a valid email.";
+	public static final String EMAIL_LONG_LOCAL = "The local part of the email address is too long. Max lengh = 64.";
+	public static final String EMAIL_LONG_DOMAIN = "The domain part of the email address is too long. Max lengh = 255.";
+	public static final String EMAIL_LONG_LABEL = "One or more labels in the domain part of the email address is too long. Max lengh = 63 each.";
+
+	// ERROR MESSAGES NAME
+
+	public static final String WRONG_LENGTH_NAME = "Name must be between " + NAME_MIN_LENGTH + " and " + NAME_MAX_LENGTH
+			+ " characters long.";
+	public static final String BAD_NAME = "Please use only letters and spaces for the name";
+
+	public Utils() {
+	}
+
+	public static boolean isFieldNull(String field) {
+		return field == null;
+	}
+
+	public static boolean isFieldEmpty(String field) {
+		return field.isBlank();
+	}
+
+	public static boolean areFieldsEqual(String field1, String field2) {
+		return field1.equals(field2);
+	}
+
+	public static String isUsernameValid(String username) {
+		if (USERNAME_MIN_LENGTH > username.length() || USERNAME_MAX_LENGTH < username.length()) {
+			return WRONG_LENGTH_USERNAME;
+		}
+
+		Pattern pattern = Pattern.compile("[a-z0-9[-_.]]+");
+		Matcher matcher = pattern.matcher(username);
+		if (!matcher.matches()) {
+			return BAD_USERNAME;
+		}
+		return SUCCESS;
+	}
+
+	public static String isPasswordValid(String password) {
+
+		if (password.length() < PASSWORD_MIN_LENGTH || password.length() > PASSWORD_MAX_LENGTH) {
+			return WRONG_LENGTH_PASSWORD;
+		}
+
+		Pattern pattern1 = Pattern.compile("[A-Z]");
+		Matcher matcher1 = pattern1.matcher(password);
+		Pattern pattern2 = Pattern.compile("[a-z]");
+		Matcher matcher2 = pattern2.matcher(password);
+		Pattern pattern3 = Pattern.compile("[0-9]");
+		Matcher matcher3 = pattern3.matcher(password);
+		Pattern pattern4 = Pattern.compile("[!@#$%^&*(),.?\":{}|<>]");
+		Matcher matcher4 = pattern4.matcher(password);
+
+		if (!matcher1.find()) {
+			return PASSWORD_NO_CAPS;
+		}
+		if (!matcher2.find()) {
+			return PASSWORD_NO_LOWKEY;
+		}
+		if (!matcher3.find()) {
+			return PASSWORD_NO_NUMBER;
+		}
+		if (!matcher4.find()) {
+			return PASSWORD_NO_SPECIAL;
+		}
+		return SUCCESS;
+	}
+
+	public static String isEmailValid(String mail) {
+		Pattern pattern = Pattern.compile("^(?![.])[a-zA-Z0-9_.]*[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+[.][a-zA-Z0-9]+",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(mail);
+		String[] domains = mail.split("@");
+		String[] labels = domains[1].split(".");
+		boolean allLabelsUnder63 = true;
+		for (int i = 0; i < labels.length; i++) {
+			if (labels[i].length() > 63) {
+				allLabelsUnder63 = false;
+			}
+		}
+		if (!matcher.matches()) {
+			return BAD_EMAIL;
+		}
+		if (domains[0].length() > 64) {
+			return EMAIL_LONG_LOCAL;
+		}
+		if (domains[1].length() > 255) {
+			return EMAIL_LONG_DOMAIN;
+		}
+		if (!allLabelsUnder63) {
+			return EMAIL_LONG_LABEL;
+		}
+
+		return SUCCESS;
+	}
+
+	public static String isNameValid(String name) {
+		if (NAME_MIN_LENGTH > name.length() || NAME_MAX_LENGTH < name.length()) {
+			return WRONG_LENGTH_NAME;
+		}
+
+		Pattern pattern = Pattern.compile("[[a-z]+[ ]*]*", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(name);
+
+		if (!matcher.matches()) {
+			return BAD_NAME;
+		}
+		return SUCCESS;
+	}
+
+}
